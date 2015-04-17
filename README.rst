1de667f2bc0b8db74c6ac9049a127de3386bfdb4
Jon Stutters <j.stutters@ucl.ac.uk>
2015-04-17 10:56:01 +0100

    Preparation for PyPI upload

---
+++ README.rst
@@ -0,0 +1,4 @@
+toolaudit
+=========
+
+Report on the tools used in your software pipeline
--- setup.py
+++ setup.py
@@ -4,59 +4,50 @@
 Setup file for toolaudit.
 """
 
-__author__ = "Jon Stutters"
-__copyright__ = "Copyright 2015, Jon Stutters"
-__date__ = "$Date: 2015-03-18 00:03:36 +0000 (Wed, 18 Mar 2015) $".split()[1]
-
+import codecs
+import io
+import os
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
-import sys
 import toolaudit
 
 
-class Tox(TestCommand):
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.tox_args = None
+here = os.path.abspath(os.path.dirname(__file__))
 
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
 
-    def run_tests(self):
-        import tox
-        import shlex
-        errno = tox.cmdline(args=shlex.split(self.tox_args))
-        sys.exit(errno)
+def read(filename):
+    """
+    Read the contents of the files listed in filenames and return it as a
+    string.
+    """
+    return codecs.open(os.path.join(here, filename), 'r').read()
 
 setup(
     name='toolaudit',
     version=toolaudit.__version__,
     packages=['toolaudit'],
     install_requires=[
-        "alabaster==0.7.2",
-        "argparse==1.3.0",
-        "Babel==1.3",
-        "docutils==0.12",
-        "Jinja2==2.7.3",
-        "MarkupSafe==0.23",
-        "numpydoc==0.5",
-        "py==1.4.26",
-        "Pygments==2.0.2",
-        "pytest==2.6.4",
-        "pytz==2014.10",
-        "PyYAML==3.11",
-        "six==1.9.0",
-        "snowballstemmer==1.2.0",
-        "Sphinx==1.3.1",
-        "sphinx-rtd-theme==0.1.7"
+        "argparse>=1.3.0",
+        "PyYAML>=3.11",
     ],
-    tests_require=['tox'],
-    cmdclass={'test': Tox},
     entry_points={
         'console_scripts': [
             'toolaudit = toolaudit:main',
         ],
     },
+    author='Jon Stutters',
+    author_email='j.stutters@ucl.ac.uk',
+    description='Report on the tools used in your software pipeline.',
+    long_description=read('README.rst'),
+    url='https://github.com/jstutters/toolaudit',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 3 - Alpha',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Topic :: System :: Systems Administration'
+    ]
 )
